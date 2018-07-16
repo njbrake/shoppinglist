afec859fc91aeae138ec3b9d3fe0437683b2f2a4
Nate Brake <njbrake@gmail.com>
2018-07-16 19:50:06 -0400

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const items = require('./routes/api/items');
 
 const app = express();
@@ -9,6 +9,8 @@ const app = express();
 //Body Parser Middleware
 
 app.use(bodyParser.json());
+
+
 
 //DB config
 const db = require('./config/keys').mongoURI;
@@ -22,6 +24,16 @@ mongoose
 //Use Routes
 app.use('/api/items', items);
 
+//Serve Static Assets if in production
+if(process.env.NODE_ENV === 'production'){
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
